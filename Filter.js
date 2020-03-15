537a35ee46b50534b01f7b78fb61907f76f9e805
Asher Morgan <Asherfmorgan@gmail.com>
2020-03-15 09:11:45 -0700

    Implement 2FieldsAll filter.

--- Filter.js
+++ Filter.js
@@ -62,15 +62,15 @@ class Filter
         {
             case "None":
                 return new Filter([], []);
+
+            case "2FieldsAll":
+                return new Filter([[0,1], [1,0]], []);
             
             case "VerbsConjugations":
                 return new Filter([[0,2], [0,4], [0,5], [0,6], [0,7], [0,8], [0,10], [0,11], [0,12], [0,13], [0,14], [0,16], [0,17], [0,18], [0,19], [0,20]], []);
             
             case "VerbsEnglish":
                 return new Filter([[2,0], [4,0], [5,0], [6,0], [7,0], [8,0], [10,0], [11,0], [12,0], [13,0], [14,0], [16,0], [17,0], [18,0], [19,0], [20,0]], []);
-
-            case "VerbDefinitions":
-                return new Filter([[0,1], [1,0]], []);
 
             case "VerbPresentParticiples":
                 return new Filter([[0,2]], []);
