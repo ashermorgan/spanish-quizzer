ff928c6d3397c2b9e442b1ada1445460dfe12415
AsherMorgan <59518073+AsherMorgan@users.noreply.github.com>
2020-09-03 10:16:08 -0700

    Implement Vue in offline instructions.

---
+++ Scripts/Offline.js
@@ -0,0 +1,49 @@
+// Declare global variables
+let app;
+
+
+
+// Load the document
+function Load() {
+    // Initialize Vue
+    app = new Vue({
+        el: "#app", // Mount to app div
+
+        data: {
+            darkTheme: false
+        },
+
+        watch: {
+            darkTheme: function() {
+                // Get theme from localStorage if null
+                if (this.darkTheme === null) {
+                    this.darkTheme = JSON.parse(localStorage.getItem("darkTheme"));
+                }
+
+                // Detect preferred color scheme if null
+                if (this.darkTheme === null) {
+                    this.darkTheme = (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches)
+                }
+
+                // Apply theme
+                if (this.darkTheme) {
+                    document.body.classList.add("dark");
+                }
+                else {
+                    document.body.classList.remove("dark");
+                }
+
+                // Save theme
+                localStorage.setItem("darkTheme", this.darkTheme);
+            }
+        }
+    });
+
+    // Load settings
+    app.darkTheme = null;   // Force theme to update
+
+    // Add event Listeners
+    document.addEventListener("click", function (e) {
+        document.getElementById('share').hidden = true;
+    });
+}
