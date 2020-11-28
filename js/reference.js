da409182a3d8961b353ee5fdf2212a6b006117a6
AsherMorgan <59518073+AsherMorgan@users.noreply.github.com>
2020-11-28 14:41:23 -0800

    Implement "All Sets" filter in reference tables.

--- js/reference.js
+++ js/reference.js
@@ -12,7 +12,7 @@ function loadVue() {
 
         data: {
             set: "Choose a vocab set",
-            sets: {"Choose a vocab set":[]},
+            sets: {"Choose a vocab set":[], "All Sets":[["English", "Spanish", "Type"]]},
             query: ""
         }
     });
@@ -48,8 +48,25 @@ function Load() {
         Papa.parse(`vocab/${setName}.csv`, {
             download: true,
             complete: function(results) {
-                // Set verbs
+                // Add Set
                 app.sets[setName] = results.data;
+
+                // Add data to "All Sets"
+                if (setName === "Verbs") {
+                    for (let row of results.data.slice(1)) {
+                        app.sets["All Sets"].push([row[0], row[1], "Verb"]);
+                    }
+                }
+                else {
+                    app.sets["All Sets"].push(...results.data.slice(1));
+                }
+
+                // Sort "All Sets"
+                app.sets["All Sets"].sort(function(a, b) {
+                    if (a[0] === "English") return false;       // Header row should be at the top
+                    else if (b[0] === "English") return true;   // Header row should be at the top
+                    else return a[0] > b[0];                    // Sort other rows by 1st item
+                })
             }
         });
     }
